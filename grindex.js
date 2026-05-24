eb6a807a89d30be59cf6b7624445fb7e12616608
Chinaa <chinaa@MacBook-Pro.local>
2026-05-24 13:19:23 +0000

    learning notation types in js

--- grindex.js
+++ grindex.js
@@ -2,3 +2,11 @@ let person = {
     name: 'chinaa🍧🍉',
     age: 2000
 };
+
+//Dot notation
+person.name = 'riza';
+console.log(person.name);
+
+//Bracket notation
+person ['name'] = 'Esther';
+console.log(person['name']);
