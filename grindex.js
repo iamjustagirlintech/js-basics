c3014a47bae6b6a9a3c13e888953768bc8f7605f
Chinaa <chinaa@MacBook-Pro.local>
2026-05-24 13:08:20 +0000

    learning consts in js

--- grindex.js
+++ grindex.js
@@ -1,7 +1,2 @@
-let name = 'chinaa🍧🍉';
-console.log(name);
-
-//a new comment, below is the new way of declaring a variable, that has multiple value.
-
-let firstName = 'chinaa🍧🍉';
-let lastName = 'Emmanuel';
+const interestRate = 56.8;
+console.log(interestRate);
